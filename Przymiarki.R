6fdb896fdbe0f345d4692aa23aaab1f34e9cc61e
KP ASUS <kpiatkowski1@swps.edu.pl>
2023-11-29 14:59:34 +0100

    inner product

--- Przymiarki.R
+++ Przymiarki.R
@@ -11,17 +11,20 @@ vector1 <- sign(rnorm(features))
 vector2 <- sign(rnorm(features))
 vector1[vector1 == -1] <- 0
 vector2[vector2 == -1] <- 0
+
+vector1
+vector2
 # docelowo wektory mają być pomiędzy 0 a 1
 # w tym celu, prawdopodobnie trzeba będzie je znormalizować/wystandaryzować
 # https://www.simplilearn.com/normalization-vs-standardization-article
 
   # trzy sposoby na policzenie outer product
+  # metoda1
   o1 <- outer(vector1, vector2)
+  #metoda2
   o2 <- vector1 %o% vector2
   
-  vector1[1]
-  vector2[1]
-  
+  #metoda3
   mat <- matrix(rep(0,features^2),nrow = features)
   
   
@@ -38,3 +41,21 @@ vector2[vector2 == -1] <- 0
   o2
   o3
 
+  #inner product
+  #metoda1
+  i1 <- o1 %*% vector1
+  
+  #metoda2
+  s <- c()
+  for (i in 1:features) {
+    su <- 0
+    for (j in 1:features) {
+      su <- su + (vector1[j] * o1[i,j])
+    }
+    s[i] <- su
+  }
+  i2 <- s
+
+  i1
+  i2
+  
